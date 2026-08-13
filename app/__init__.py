cb88e9f4c1c75902dabae1e8506365da2f8e00e4
miftah al rasyid <miftahalrasyid@users.noreply.github.com>
2026-08-13 11:22:52 +0900

    fix(app) update: fix route imports and add dotenv library

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
+import os
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
+from dotenv import load_dotenv
 
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
 def create_app():
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/revoshop_db'
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
