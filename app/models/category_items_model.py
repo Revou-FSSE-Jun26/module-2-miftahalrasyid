e5e807d0d375a638d8083d449dd168c26ed63727
miftah al rasyid <miftahalrasyid@users.noreply.github.com>
2026-08-20 13:21:45 +0900

    feat(category_item): change the craeted at to server_default

--- app/models/category_items_model.py
+++ app/models/category_items_model.py
@@ -1,9 +1,9 @@
-from app import db
-import datetime as dt
+from app.extensions import db
+from sqlalchemy import func
 
 category_items = db.Table(
     'category_items',
     db.Column('category_id', db.Integer, db.ForeignKey('categories.id', ondelete='CASCADE'), primary_key=True),
     db.Column('product_id', db.Integer, db.ForeignKey('products.id', ondelete='CASCADE'), primary_key=True),
-    db.Column('created_at', db.DateTime(timezone=True), default=lambda: dt.datetime.now(dt.timezone.utc))
+    db.Column('created_at', db.DateTime(timezone=True), server_default=func.now())
 )
